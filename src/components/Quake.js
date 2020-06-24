fabff8b2623f0637bb9becc0d4740e58f023f682
Myrtha Voges <myrtha.voges@21re.de>
2020-06-24 12:47:09 +0200

    Shake cards on hover according to quake severity

--- src/components/Quake.js
+++ src/components/Quake.js
@@ -4,12 +4,22 @@ import '../styles/app.css'
 export default function Quake({ data }) {
 
     const time = new Date(data.properties.time)
+    const severity = Math.pow(data.properties.mag + 1, 3)
+
+    console.log("magnitude:", data.properties.mag)
+    console.log("severity:", severity)
+
+    let customCssProperties = {}
+    customCssProperties['--severityHigh'] = `${severity}px`
+    customCssProperties['--severityLow'] = `-${severity}px`
 
     return (
-        <div className="card">
+        <div className="card" style={customCssProperties}>
             <b>{data.properties.place}:</b>
             <br/>
             at {time.toLocaleTimeString()}
+            <br/>
+            magnitude: {data.properties.mag}
         </div>
     )
 }
